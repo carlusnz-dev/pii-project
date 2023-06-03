6423306ed73b8ec69b8ec77d5d69ccabd8c8b43d
carlusnz-dev <carlosantunes.dev@gmail.com>
2023-06-03 16:51:17 -0300

    first commit

--- script/script.js
+++ script/script.js
@@ -19,4 +19,7 @@ $('#recipeCarousel').carousel({
          next.children(':first-child').clone().appendTo($(this));
        }
  });
+
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
  
